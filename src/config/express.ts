3210873c74e4a6170033c72409051bec0613f48b
Vachter Roland <roland.vachter@nagarro.com>
2026-03-09 15:05:17 +0200

    Fix view path.

--- src/config/express.ts
+++ src/config/express.ts
@@ -25,7 +25,7 @@ function initExpress(): Server {
     defaultLayout: 'main',
     extname: '.handlebars'
   }));
-  app.set('views', path.join(__dirname, (__dirname.includes('dist') ? '../' : '') + '../views'));
+  app.set('views', path.join(__dirname, (__dirname.includes('dist') ? '../src/' : '../') + 'views'));
   app.set('view engine', 'handlebars');
 
   // Middleware
